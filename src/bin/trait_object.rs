e2153b080e36bf8fcbd6d63e46c3cdc6edc2c74e
deepgreenAN <asami73dgreen63@gmail.com>
2023-03-14 20:04:33 +0900

    modify trait object

--- src/bin/trait_object.rs
+++ src/bin/trait_object.rs
@@ -1,18 +1,19 @@
 fn main() {
     use enum_dispatch_examples::{PushPop, Queue, Stack};
-    let mut collection: Box<dyn PushPop<i32>> = Box::new(Stack::new());
-    collection.push(1);
-    collection.push(2);
-    collection.push(3);
+    let mut collections: Vec<Box<dyn PushPop<i32>>> =
+        vec![Box::new(Stack::new()), Box::new(Queue::new())];
 
-    let popped_item = collection.pop();
+    collections[0].push(1);
+    collections[0].push(2);
+    collections[0].push(3);
+
+    let popped_item = collections[0].pop();
     println!("popped item: {popped_item:?}");
 
-    let mut collection: Box<dyn PushPop<i32>> = Box::new(Queue::new());
-    collection.push(1);
-    collection.push(2);
-    collection.push(3);
+    collections[1].push(1);
+    collections[1].push(2);
+    collections[1].push(3);
 
-    let popped_item = collection.pop();
+    let popped_item = collections[1].pop();
     println!("popped item: {popped_item:?}");
 }
